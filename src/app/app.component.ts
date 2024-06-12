06b73533c0fb3147197e8e7ba631900dec2fadac
John Kirathe <jnkratz@gmail.com>
2024-06-12 15:54:54 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  <h1>{{ title }}</h1>
+  <p>User Count: {{ userCount() }}</p>`,
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drag-drop-upload-app';
+  private users = signal<User[]>([]);
+  userCount = computed(() => this.users().length);
+
+  ngOnInit(): void {
+    this.users.set([{ id: 1, name: 'john', email: 'john@mail.com' }]);
+
+    this.users.update((users: User[]) => [
+      ...users,
+      { id: 2, name: 'john2', email: 'john2@mail.com' },{ id: 3, name: 'john', email: 'john@mail.com' }
+    ]);
+
+    this.users.update((users: User[]) =>
+      users.map((user) => {
+        if (user.id === 1) {
+          user.email = 'j@mail.com';
+        }
+        return user;
+      })
+    );
+
+    console.log('userCount: ', this.userCount());
+
+    const showCount = signal(true);
+    const count = signal(10);
+    const conditionalCount = computed(() => {
+      if (showCount()) {
+        return `The count is ${count()}.`;
+      } else {
+        return 'Nothing to see here!';
+      }
+    });
+
+    console.log('conditionalCount: ', conditionalCount());
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
